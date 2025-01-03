8723037e4771fdff7a004db98cbcbfcec5285fd5
Santiago Sala <pipo0716@gmail.com>
2025-01-03 14:04:14 +0200

    2,15

--- Scripts/SpeedySaucer_Scripts/player.gd
+++ Scripts/SpeedySaucer_Scripts/player.gd
@@ -2,4 +2,12 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	apply_impulse(Vector2(30, 50))
+#	apply_impulse(Vector2(30, 50))
+	pass
+func _process(delta: float) -> void:
+	print(Engine.get_frames_per_second())
+#	apply_force(Vector2(10,20))
+	
+
+func _physics_process(delta: float) -> void:
+	apply_force(Vector2(25,0))
