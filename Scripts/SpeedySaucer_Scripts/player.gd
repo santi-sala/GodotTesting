9f21e42aadb4ec36bffbf7efc41849ff3f5afe41
Santiago Sala <pipo0716@gmail.com>
2025-01-03 13:46:52 +0200

    2,12

--- Scripts/SpeedySaucer_Scripts/player.gd
+++ Scripts/SpeedySaucer_Scripts/player.gd
@@ -4,8 +4,9 @@ extends RigidBody2D
 func _ready() -> void:
 	test()
 
-var my_number = 10
+var my_number
 func test():
+	my_number = (2+3)*5
 	print("k pasa maaaan!")
 	print(str(my_number))
-	
+	print("2 + 2 is = " + str(2+2))
