43119af7d2945402212a2be56708a1cc2cdf1e90
Santiago Sala <pipo0716@gmail.com>
2025-01-03 12:54:47 +0200

    2,11

--- Scripts/SpeedySaucer_Scripts/player.gd
+++ Scripts/SpeedySaucer_Scripts/player.gd
@@ -1,11 +1,11 @@
 extends RigidBody2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	test()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var my_number = 10
+func test():
+	print("k pasa maaaan!")
+	print(str(my_number))
+	
