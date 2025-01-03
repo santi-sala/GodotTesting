5c0d6a9076ccb94aaa2f83a418fbc508927b984a
Santiago Sala <pipo0716@gmail.com>
2025-01-03 13:55:01 +0200

    2,14

--- Scripts/SpeedySaucer_Scripts/player.gd
+++ Scripts/SpeedySaucer_Scripts/player.gd
@@ -2,11 +2,4 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	test(my_number)
-
-var my_number = (2+3)*5
-
-func test(number):
-	print("k pasa maaaan!")
-	print(str(number * 2))
-	print("2 + 2 is = " + str(2+2))
+	apply_impulse(Vector2(30, 50))
