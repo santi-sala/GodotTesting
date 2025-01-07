cae558a7014e7d84af56221c4b4ee69f4f8820cb
Santiago Sala <pipo0716@gmail.com>
2025-01-07 15:52:18 +0200

    2,23

--- Scripts/SpeedySaucer_Scripts/speedy_saucer_level.gd
+++ Scripts/SpeedySaucer_Scripts/speedy_saucer_level.gd
@@ -4,3 +4,11 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("K pasa world!!")
+
+
+func _on_maze_body_exited(body: Node2D) -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_maze_body_entered(body: Node2D) -> void:
+	print("in!")
