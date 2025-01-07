bc4a73853dc66340530204f80e99c3fd1a860cd9
Santiago Sala <pipo0716@gmail.com>
2025-01-07 14:25:25 +0200

    2,16

--- Scripts/SpeedySaucer_Scripts/player.gd
+++ Scripts/SpeedySaucer_Scripts/player.gd
@@ -1,12 +1,20 @@
 extends RigidBody2D
-
+var my_string = "Sup"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 #	apply_impulse(Vector2(30, 50))
-	pass
+	if my_string == "Sup":
+		print("Sup triggered")
+	elif my_string == "K pasa":
+		print(" K pasa triggered")
+	else:
+		print("Wrong trigger")
+		 
+	
 func _process(delta: float) -> void:
-	print(Engine.get_frames_per_second())
+	#print(Engine.get_frames_per_second())
 #	apply_force(Vector2(10,20))
+	pass
 	
 
 func _physics_process(delta: float) -> void:
