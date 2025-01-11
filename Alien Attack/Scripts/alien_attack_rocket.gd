a2dd0bf5b8879d8df0fe058de18784882dd5542d
Santiago Sala <pipo0716@gmail.com>
2025-01-11 10:49:23 +0200

    3,9

--- Alien Attack/Scripts/alien_attack_rocket.gd
+++ Alien Attack/Scripts/alien_attack_rocket.gd
@@ -2,6 +2,13 @@ extends Area2D
 	
 @export var speed = 500
 
+@onready var rocket_screen_notifier = $VisibleOnScreenNotifier2D
+
+func _ready() -> void:
+	rocket_screen_notifier.connect("screen_exited", _on_rocket_exited_screen)
+
 func _physics_process(delta: float) -> void:
 	global_position.x += speed * delta
 	
+func _on_rocket_exited_screen():
+	queue_free()
